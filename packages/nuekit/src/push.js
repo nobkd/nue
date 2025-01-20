d0427505ec3c1e79f8f3b03045cd5528bb953c19
nobkd <44443899+nobkd@users.noreply.github.com>
2025-01-20 19:10:31 +0100

    more

--- packages/nuekit/src/push.js
+++ packages/nuekit/src/push.js
@@ -1,12 +1,31 @@
 export async function findUserData() {
-  // TODO: find username, check for ssh key, check if remote user repo exists
-
-  return {
-    base: null, // set, if not on <username>.github.io but <username>.github.io/<somedir>
+  data = {
     is_prod: true, // always prod
   }
+
+  // TODO: find username, check for ssh key, check if remote user repo exists
+
+
+  if (!data.user) {
+    // ask user for uname
+  } else {
+    // found <uname>, do you agree?
+  }
+
+  data.repo = `${uname}.github.io`
+  
+  // ask if user agrees with repo
+  // else use provided repo and set `base` to subpath
+  // data.base = null // set, if not on <username>.github.io but <username>.github.io/<somedir>
+
+  // check if repo already exists, etc.
+  fetch(`https://api.github.com/repos/${data.user}/${data.repo}`)
+
+  // more
+
+  return data
 }
 
-export async function push(data, root){
+export async function push(data, root) {
   // TODO: handle pushing to remote, finalizing steps to publish page
 }
